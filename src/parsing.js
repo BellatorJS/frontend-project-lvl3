d50bec16725e96f9f8f01ebabf5f6592f86b0bd7
Aleksey <bellatorjs1@gmail.com>
2022-07-20 22:25:23 +0300

    4 step

--- src/parsing.js
+++ src/parsing.js
@@ -1,12 +1,19 @@
 import axios from 'axios';
 
+const parsingHtml = (datas) => {
+  const domparser = new DOMParser();
+  const html = domparser.parseFromString(datas, 'text/xml');
+  return html;
+};
 const parsing = (rssLink) => {
   const instance = axios.create();
   instance.defaults.timeout = 2500;
-  const x = instance.get(rssLink)
-    .then((response) => console.log(response.data));
-
-  return x;
+  const result = instance.get(rssLink)
+    .then((response) => response.data)
+    .then((response1) => parsingHtml(response1))
+    .then((data) => (data))
+    .catch(() => console.log('!!!!'));
+  return result;
 };
 
 export default parsing;
--- src/view.js
+++ src/view.js
@@ -27,16 +27,54 @@ const foo = (state) => {
     inputform.value = '';
     inputform.focus();
   };
+  const renderRSS = (promise) => {
+    console.log(promise);
+    // const feedDescription = promise.querySelector('description').textContent;
+    // const feedTitle = promise.querySelector('title').textContent;
+    const items = Array.from(promise.querySelectorAll('item'));
+    items.map((item) => {
+      const title = item.querySelector('title').textContent;
+      const link = item.querySelector('link').textContent;
+      const description = item.querySelector('description').textContent;
+      console.log(link);
+      console.log(title);
+      console.log(description);
+    });
+    const cardPosts = document.createElement('div');
+    cardPosts.className = 'card border-0';
+
+    const cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+    const postName = document.createElement('h2');
+    postName.className = 'card-title m4';
+    postName.textContent = 'Посты';
+    cardPosts.append(cardBody);
+    cardBody.append(postName);
+    const posts = document.querySelector('.posts');
+    const ul = document.createElement('ul');
+    ul.className = 'list-group border-0 rounded-0';
+    const li = document.createElement('li');
+    li.className = 'list-group-item d-flex justify-content-beetwen align-items-start border-0 border-end-0';
+    const a = document.createElement('a');
+    a.className = 'fw-bold';
+    a.setAttribute('data-id', '');
+    const btn = document.createElement('button');
+    btn.setAttribute('type', 'button');
+    btn.className = 'btn btn-outline-primary btn-sm';
+
+    posts.append(cardPosts);
+  };
   const watchedState = onChange(state, (path, value) => {
     console.log(value);
     switch (path) {
       case 'urlLinks':
-        const [value1] = value;
-        parsing(value1);
+        const [data] = value;
+        const x = parsing(data);
+        x.then((d) => renderRSS(d));
+
         renderLinks();
         break;
       case 'errors':
-
         renderErrors();
         break;
       default:
